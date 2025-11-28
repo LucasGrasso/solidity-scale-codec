771c0528004fb856a14042b77a5a3af390e717de
LucasGrasso <lucasgrassoramos@gmail.com>
2025-11-27 23:11:50 -0300

    refactor example

--- contracts/examples/Example.sol
+++ contracts/examples/Example.sol
@@ -1,14 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
-import {ScaleCodecU8, ScaleCodecU16, ScaleCodecI16, ScaleCodecBool} from "../libraries/ScaleCodec/ScaleCodec.sol";
+import {ScaleCodec} from "../libraries/ScaleCodec/ScaleCodec.sol";
 
 contract Example {
-    using ScaleCodecBool for bool;
-    using ScaleCodecU8 for uint8;
-    using ScaleCodecU16 for uint16;
-    using ScaleCodecI16 for int16;
-
     struct Foo {
         uint8 a;
         uint16 b;
@@ -19,10 +14,10 @@ contract Example {
     function encodeFoo(Foo calldata f) external pure returns (bytes memory) {
         return
             abi.encodePacked(
-                f.a.encode(),
-                f.b.encode(),
-                f.c.encode(),
-                f.d.encode()
+                ScaleCodec.encodeU8(f.a),
+                ScaleCodec.encodeU16(f.b),
+                ScaleCodec.encodeI16(f.c),
+                ScaleCodec.encodeBool(f.d)
             );
     }
 }
