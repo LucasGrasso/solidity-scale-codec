ee079f9a8c484b795f6d6d6d15d830816c07dcdb
LucasGrasso <lucasgrassoramos@gmail.com>
2026-03-29 19:20:21 -0300

    add comp information to constants

--- src/Xcm/v5/Constants.sol
+++ src/Xcm/v5/Constants.sol
@@ -1,4 +1,7 @@
-/// @notice Maximum number of items we expect in a single Assets value.
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.28;
+
+// Maximum number of items we expect in a single Assets value.
 uint64 constant MAX_ITEMS_IN_ASSETS = 20;
-/// @notice Maximum number of PalletsInfo entries we expect in a single `PalletsInfo` value.
+// Maximum number of PalletsInfo entries we expect in a single `PalletsInfo` value.
 uint32 constant MAX_PALLETS_INFO = 64;
