393ecf7afc9880ef92beafb9152cffd0a4d44863
LucasGrasso <lucasgrassoramos@gmail.com>
2026-03-30 14:11:25 -0300

    Refactor Xcm `QueryResponseInfo`

--- src/Xcm/v5/QueryResponseInfo/QueryResponseInfoCodec.sol
+++ src/Xcm/v5/QueryResponseInfo/QueryResponseInfoCodec.sol
@@ -1,20 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.28;
 
-import {LocationCodec, Location} from "./Location.sol";
-import {WeightCodec, Weight} from "./Weight.sol";
-import {QueryId} from "./Types/QueryId.sol";
-import {Compact} from "../../Scale/Compact.sol";
-
-/// @notice Information regarding the composition of a query response.
-struct QueryResponseInfo {
-    /// @custom:property The destination to which the query response message should be sent.
-    Location destination;
-    /// @custom:property The `query_id` field of the `QueryResponse` message.
-    QueryId queryId;
-    /// @custom:property The `max_weight` field of the `QueryResponse` message.
-    Weight maxWeight;
-}
+import {LocationCodec, Location} from "../Location.sol";
+import {WeightCodec, Weight} from "../Weight.sol";
+import {QueryId} from "../Types/QueryId.sol";
+import {Compact} from "../../../Scale/Compact.sol";
+import {QueryResponseInfo} from "./QueryResponseInfo.sol";
 
 /// @title SCALE Codec for XCM v5 `QueryResponseInfo`
 /// @notice SCALE-compliant encoder/decoder for the `QueryResponseInfo` type.
