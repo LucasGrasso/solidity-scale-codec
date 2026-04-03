ba539cf3ef4dea43342c2aef1a7959bcfb458684
LucasGrasso <lucasgrassoramos@gmail.com>
2026-04-03 18:31:53 -0300

    decoding test

--- test/VersionedXcm.t.sol
+++ test/VersionedXcm.t.sol
@@ -3,12 +3,23 @@ pragma solidity ^0.8.28;
 
 import {VersionedXcmCodec} from "../src/Xcm/VersionedXcm/VersionedXcmCodec.sol";
 import {VersionedXcm} from "../src/Xcm/VersionedXcm/VersionedXcm.sol";
-
 import {XcmCodec} from "../src/Xcm/v5/Xcm/XcmCodec.sol";
 import {Xcm} from "../src/Xcm/v5/Xcm/Xcm.sol";
+import {Instruction, WithdrawAssetParams, BuyExecutionParams, DepositAssetParams} from "../src/Xcm/v5/Instruction/Instruction.sol";
+import {InstructionCodec} from "../src/Xcm/v5/Instruction/InstructionCodec.sol";
+import {Asset} from "../src/Xcm/v5/Asset/Asset.sol";
+import {Location, parent} from "../src/Xcm/v5/Location/Location.sol";
+import {AssetId} from "../src/Xcm/v5/AssetId/AssetId.sol";
+import {fungible, FungibleParams} from "../src/Xcm/v5/Fungibility/Fungibility.sol";
+import {unlimited} from "../src/Xcm/v5/WeightLimit/WeightLimit.sol";
+import {AssetFilter, wild, WildParams} from "../src/Xcm/v5/AssetFilter/AssetFilter.sol";
+import {allOf, AllOfParams} from "../src/Xcm/v5/WildAsset/WildAsset.sol";
+import {WildFungibility} from "../src/Xcm/v5/WildFungibility/WildFungibility.sol";
+import {accountId32, AccountId32Params} from "../src/Xcm/v5/Junction/Junction.sol";
+import {fromJunction} from "../src/Xcm/v5/Junctions/Junctions.sol";
+import {polkadot} from "../src/Xcm/v5/NetworkId/NetworkId.sol";
 
 import {Test} from "forge-std/Test.sol";
-import "forge-std/console.sol";
 
 contract VersionedXcmWrapper {
     function decode(
@@ -21,6 +32,8 @@ contract VersionedXcmWrapper {
 contract VersionedXcmTest is Test {
     VersionedXcmWrapper wrapper;
 
+    using InstructionCodec for Instruction;
+
     function setUp() public {
         wrapper = new VersionedXcmWrapper();
     }
@@ -30,20 +43,74 @@ contract VersionedXcmTest is Test {
         bytes
             memory encoded = hex"050c000401000003008c86471301000003008c8647000d010101000000010100368e8759910dab756d344995f1d3c79374ca8f70066d3a709e48029f6bf0ee7e";
         VersionedXcm memory xcm = wrapper.decode(encoded);
-        console.log("Decoded versioned XCM:");
-        console.log(" - Type: ", uint256(xcm.version));
 
         Xcm memory xcmV5 = VersionedXcmCodec.asV5(xcm);
-        console.log("Decoded XCM V5:");
         uint256 instructionsCount = xcmV5.instructions.length;
-        for (uint256 i = 0; i < instructionsCount; i++) {
-            console.log(
-                "   - Instruction ",
-                i,
-                ": ",
-                uint256(xcmV5.instructions[i].variant)
-            );
-            // Decode each instruction
-        }
+        assertEq(instructionsCount, 3);
+
+        // Decode the first instruction and check its type
+
+        WithdrawAssetParams memory params = xcmV5
+            .instructions[0]
+            .asWithdrawAsset();
+        assertEq(params.assets.items.length, 1);
+
+        Asset memory expectedAsset = Asset({
+            id: AssetId({location: parent()}),
+            fungibility: fungible(FungibleParams({amount: 1200000000}))
+        });
+        assertEq(
+            keccak256(abi.encode(params.assets.items[0])),
+            keccak256(abi.encode(expectedAsset))
+        );
+
+        BuyExecutionParams memory buyParams = xcmV5
+            .instructions[1]
+            .asBuyExecution();
+        assertEq(
+            keccak256(abi.encode(buyParams.fees)),
+            keccak256(abi.encode(expectedAsset))
+        );
+        assertEq(
+            keccak256(abi.encode(buyParams.weightLimit)),
+            keccak256(abi.encode(unlimited()))
+        );
+
+        DepositAssetParams memory depositParams = xcmV5
+            .instructions[2]
+            .asDepositAsset();
+
+        AssetFilter memory expectedFilter = wild(
+            WildParams({
+                wildAsset: allOf(
+                    AllOfParams({
+                        id: AssetId({location: parent()}),
+                        fun: WildFungibility.Fungible
+                    })
+                )
+            })
+        );
+        assertEq(
+            keccak256(abi.encode(depositParams.assets)),
+            keccak256(abi.encode(expectedFilter))
+        );
+
+        Location memory expectedBeneficiary = Location({
+            parents: 0,
+            interior: fromJunction(
+                accountId32(
+                    AccountId32Params({
+                        hasNetwork: false,
+                        id: hex"368e8759910dab756d344995f1d3c79374ca8f70066d3a709e48029f6bf0ee7e",
+                        network: polkadot() // just for clarity, since hasNetwork is false, this field is not actually encoded in the junction
+                    })
+                )
+            )
+        });
+
+        assertEq(
+            keccak256(abi.encode(depositParams.beneficiary)),
+            keccak256(abi.encode(expectedBeneficiary))
+        );
     }
 }
