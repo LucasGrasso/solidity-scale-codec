b35f250888137180e46e0b244bbccf4eea72e750
LucasGrasso <lucasgrassoramos@gmail.com>
2026-04-03 02:14:30 -0300

    refactor `BodyPart`

--- src/Xcm/v5/BodyPart/BodyPart.sol
+++ src/Xcm/v5/BodyPart/BodyPart.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.28;
 import {Compact} from "../../../Scale/Compact.sol";
 
 /// @dev Discriminant for the different types of BodyParts in XCM v5. Each variant corresponds to a specific structure of the payload.
-enum BodyPartType {
+enum BodyPartVariant {
     /// @custom:variant The body's declaration, under whatever means it decides.
     Voice,
     /// @custom:variant A given number of members of the body.
@@ -19,8 +19,8 @@ enum BodyPartType {
 
 /// @notice A part of a pluralistic body.
 struct BodyPart {
-    /// @custom:property The type of BodyPart, which determines how to interpret the payload. See `BodyPartType` enum for possible values.
-    BodyPartType bodyPartType;
+    /// @custom:property The type of BodyPart, which determines how to interpret the payload. See `BodyPartVariant` enum for possible values.
+    BodyPartVariant variant;
     /// @custom:property For Members, this will hold the count. For Fraction, AtLeastProportion, and MoreThanProportion, this will hold the encoded Proportion struct. For Voice, this will be empty.
     bytes payload;
 }
@@ -36,7 +36,7 @@ struct MembersParams {
 /// @notice Creates a `BodyPart` struct representing a `Voice` body part.
 /// @return A `BodyPart` with the `Voice` variant.
 function voice() pure returns (BodyPart memory) {
-    return BodyPart({bodyPartType: BodyPartType.Voice, payload: ""});
+    return BodyPart({variant: BodyPartVariant.Voice, payload: ""});
 }
 
 /// @notice Creates a `BodyPart` struct representing a `Members` body part with the given count.
@@ -45,7 +45,7 @@ function voice() pure returns (BodyPart memory) {
 function members(MembersParams memory params) pure returns (BodyPart memory) {
     return
         BodyPart({
-            bodyPartType: BodyPartType.Members,
+            variant: BodyPartVariant.Members,
             payload: Compact.encode(params.count)
         });
 }
@@ -60,7 +60,7 @@ function fraction(
 ) pure returns (BodyPart memory) {
     return
         BodyPart({
-            bodyPartType: BodyPartType.Fraction,
+            variant: BodyPartVariant.Fraction,
             payload: abi.encodePacked(
                 Compact.encode(nominator),
                 Compact.encode(denominator)
@@ -78,7 +78,7 @@ function atLeastProportion(
 ) pure returns (BodyPart memory) {
     return
         BodyPart({
-            bodyPartType: BodyPartType.AtLeastProportion,
+            variant: BodyPartVariant.AtLeastProportion,
             payload: abi.encodePacked(
                 Compact.encode(nominator),
                 Compact.encode(denominator)
@@ -96,7 +96,7 @@ function moreThanProportion(
 ) pure returns (BodyPart memory) {
     return
         BodyPart({
-            bodyPartType: BodyPartType.MoreThanProportion,
+            variant: BodyPartVariant.MoreThanProportion,
             payload: abi.encodePacked(
                 Compact.encode(nominator),
                 Compact.encode(denominator)
