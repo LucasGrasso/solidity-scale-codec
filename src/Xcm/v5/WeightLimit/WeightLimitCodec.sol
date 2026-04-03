a73cf7453d83a0e1c9a2fcb85fcbcc89897372f8
LucasGrasso <lucasgrassoramos@gmail.com>
2026-04-03 15:33:13 -0300

    refactor `WeightLimit`

--- src/Xcm/v5/WeightLimit/WeightLimitCodec.sol
+++ src/Xcm/v5/WeightLimit/WeightLimitCodec.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.28;
 
 import {Weight} from "../Weight/Weight.sol";
 import {WeightCodec} from "../Weight/WeightCodec.sol";
-import {WeightLimit, WeightLimitType} from "./WeightLimit.sol";
+import {WeightLimit, WeightLimitVariant, LimitedParams} from "./WeightLimit.sol";
+import {BytesUtils} from "../../../Utils/BytesUtils.sol";
 
 /// @title SCALE Codec for XCM v5 `WeightLimit`
 /// @notice SCALE-compliant encoder/decoder for the `WeightLimit` type.
@@ -11,7 +12,7 @@ import {WeightLimit, WeightLimitType} from "./WeightLimit.sol";
 /// @dev XCM v5 reference: https://paritytech.github.io/polkadot-sdk/master/staging_xcm/v5/enum.WeightLimit.html
 library WeightLimitCodec {
     error InvalidWeightLimitLength();
-    error InvalidWeightLimitType(uint8 wlType);
+    error InvalidWeightLimitVariant(uint8 variant);
 
     /// @notice Encodes a `WeightLimit` struct into SCALE bytes.
     /// @param wl The `WeightLimit` struct to encode.
@@ -19,7 +20,7 @@ library WeightLimitCodec {
     function encode(
         WeightLimit memory wl
     ) internal pure returns (bytes memory) {
-        return abi.encodePacked(uint8(wl.wlType), wl.payload);
+        return abi.encodePacked(uint8(wl.variant), wl.payload);
     }
 
     /// @notice Returns the number of bytes that a `WeightLimit` would occupy when SCALE-encoded.
@@ -31,13 +32,13 @@ library WeightLimitCodec {
         uint256 offset
     ) internal pure returns (uint256) {
         if (data.length < offset + 1) revert InvalidWeightLimitLength();
-        uint8 wlType = uint8(data[offset]);
-        if (wlType == uint8(WeightLimitType.Unlimited)) {
+        uint8 variant = uint8(data[offset]);
+        if (variant == uint8(WeightLimitVariant.Unlimited)) {
             return 1;
-        } else if (wlType == uint8(WeightLimitType.Limited)) {
+        } else if (variant == uint8(WeightLimitVariant.Limited)) {
             return 1 + WeightCodec.encodedSizeAt(data, offset + 1);
         } else {
-            revert InvalidWeightLimitType(wlType);
+            revert InvalidWeightLimitVariant(variant);
         }
     }
 
@@ -61,28 +62,36 @@ library WeightLimitCodec {
         uint256 offset
     ) internal pure returns (WeightLimit memory wl, uint256 bytesRead) {
         if (data.length < offset + 1) revert InvalidWeightLimitLength();
-        uint8 wlType = uint8(data[offset]);
-        if (wlType > uint8(WeightLimitType.Limited))
-            revert InvalidWeightLimitType(wlType);
+        uint8 variant = uint8(data[offset]);
+        if (variant > uint8(type(WeightLimitVariant).max) + 1) {
+            revert InvalidWeightLimitVariant(variant);
+        }
         uint256 size = encodedSizeAt(data, offset);
         uint256 payloadLength = size - 1;
-        bytes memory payload = new bytes(payloadLength);
-        for (uint256 i = 0; i < payloadLength; ++i) {
-            payload[i] = data[offset + 1 + i];
-        }
-        wl = WeightLimit({wlType: WeightLimitType(wlType), payload: payload});
+        bytes memory payload = BytesUtils.copy(data, offset + 1, payloadLength);
+        wl = WeightLimit({
+            variant: WeightLimitVariant(variant),
+            payload: payload
+        });
         bytesRead = size;
     }
 
     /// @notice Decodes the `Weight` from a `Limited` weight limit.
     /// @param wl The `WeightLimit` struct. Must be of type `Limited`.
-    /// @return The decoded `Weight`.
-    function asWeight(
+    /// @return params A `LimitedParams` struct containing the decoded weight.
+    function asLimited(
         WeightLimit memory wl
-    ) internal pure returns (Weight memory) {
-        if (wl.wlType != WeightLimitType.Limited)
-            revert InvalidWeightLimitType(uint8(wl.wlType));
-        (Weight memory w, ) = WeightCodec.decode(wl.payload);
-        return w;
+    ) internal pure returns (LimitedParams memory params) {
+        _assertVariant(wl, WeightLimitVariant.Limited);
+        (params.weight, ) = WeightCodec.decode(wl.payload);
+    }
+
+    function _assertVariant(
+        WeightLimit memory wl,
+        WeightLimitVariant expected
+    ) internal pure {
+        if (wl.variant != expected) {
+            revert InvalidWeightLimitVariant(uint8(wl.variant));
+        }
     }
 }
